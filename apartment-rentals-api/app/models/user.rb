2e3a653b1b4109f87ab51223c83f7b9e4778657b
Elías J. Puma <elpumitaelias@gmail.com>
2019-09-16 21:27:03 -0500

    Adds user with roles and node packages

---
+++ apartment-rentals-api/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: { admin: 0, realtor: 1, client: 2 }
+end
